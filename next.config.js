0228dff405c0f1d70cb551ba31c040780543ff1e
Matt Montgomery <mmontgomery@deseretdigital.com>
2021-10-21 13:54:14 -0600

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.txt$/,
+            loader: "emit-file-loader",
+            options: {
+              name: "dist/[path][name].[ext]",
+            },
+          },
+          {
+            test: /\.txt$/,
+            loader: "raw-loader",
+          },
+        ],
+      },
+    };
+  },
+};
